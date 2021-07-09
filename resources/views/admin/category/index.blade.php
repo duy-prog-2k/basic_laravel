ece66083e83ac060e94aab0f9aab64e7142ff160
duy-prog-2k <phamkhacduy.it@gmail.com>
2021-07-09 13:42:42 +0700

    delete comment

--- resources/views/admin/category/index.blade.php
+++ resources/views/admin/category/index.blade.php
@@ -2,18 +2,9 @@
   <x-slot name="header">
     <h2 class="font-semibold text-xl text-gray-800 leading-tight">
       All Category
-      <!-- <b style="float: right">Total User:
-            <span class="badge bg-danger"></span>
-            </b> -->
     </h2>
   </x-slot>
   <div class="py-12">
-    <!-- <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                <x-jet-welcome />
-            </div>
-        </div> -->
-
     <div class="container">
       <div class="row">
 
@@ -92,6 +83,7 @@
                   <div class="card-header">
                     Trash List
                   </div>
+
                   <table class="table">
                     <thead>
                       <tr>
@@ -127,7 +119,6 @@
               <div class="col-md-4">
 
               </div>
-
 
       </div>
     </div>
